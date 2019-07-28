4e8f490feab93728a50baae359326d5bc9841da1
zcwfeng <zcwfeng@126.com>
2019-07-28 18:46:37 +0800

    合并代码

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ void main() {
 }
 
 //dev
-class BottomNavigationWidget extends StatefulWidget{
+class BottomNavigationWidget extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => BottomNavigationWidgetState();
 }
@@ -36,111 +36,118 @@ class BottomNavigationWidgetState extends State<BottomNavigationWidget> {
   @override
   Widget build(BuildContext context) {
     return Stack(
-        children: <Widget>[
-          Offstage(
-            offstage: showAd,
-            child: Scaffold(
-              body: pages[_currentIndex],
-              bottomNavigationBar: BottomNavigationBar(
-                items: [
-                  BottomNavigationBarItem(
-                      icon: Icon(
-                        Icons.widgets,
-                        color: _bottomNavigationColor,
-                      ),
-                      title: Text(
-                        'WASTE',
-                        style: TextStyle(color: _bottomNavigationColor,fontSize: 12, fontWeight: FontWeight.bold),
-                      )),
-                  BottomNavigationBarItem(
-                      icon: Icon(
-                        Icons.message,
-                        color: _bottomNavigationColor,
-                      ),
-                      title: Text(
-                        'NEWS',
-                        style: TextStyle(color: _bottomNavigationColor,fontSize: 12, fontWeight: FontWeight.bold),
-                      )),
-                  BottomNavigationBarItem(
-                      icon: Icon(
-                        Icons.work,
-                        color: _bottomNavigationColor,
-                      ),
-                      title: Text(
-                        'WEATHER',
-                        style: TextStyle(color: _bottomNavigationColor,fontSize: 12, fontWeight: FontWeight.bold),
-                      )),
-                  BottomNavigationBarItem(
-                      icon: Icon(
-                        Icons.calendar_today,
-                        color: _bottomNavigationColor,
-                      ),
-                      title: Text(
-                        'CALENDAR',
-                        style: TextStyle(color: _bottomNavigationColor,fontSize: 12, fontWeight: FontWeight.bold),
-                      )),
-                ],
-                currentIndex: _currentIndex,
-                onTap: (int index) {
-                  setState(() {
-                    _currentIndex = index;
-                  });
-                },
-              ),
+      children: <Widget>[
+        Offstage(
+          offstage: showAd,
+          child: Scaffold(
+            body: pages[_currentIndex],
+            bottomNavigationBar: BottomNavigationBar(
+              items: [
+                BottomNavigationBarItem(
+                    icon: Icon(
+                      Icons.widgets,
+                      color: _bottomNavigationColor,
+                    ),
+                    title: Text(
+                      'WASTE',
+                      style: TextStyle(
+                          color: _bottomNavigationColor,
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold),
+                    )),
+                BottomNavigationBarItem(
+                    icon: Icon(
+                      Icons.message,
+                      color: _bottomNavigationColor,
+                    ),
+                    title: Text(
+                      'NEWS',
+                      style: TextStyle(
+                          color: _bottomNavigationColor,
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold),
+                    )),
+                BottomNavigationBarItem(
+                    icon: Icon(
+                      Icons.work,
+                      color: _bottomNavigationColor,
+                    ),
+                    title: Text(
+                      'WEATHER',
+                      style: TextStyle(
+                          color: _bottomNavigationColor,
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold),
+                    )),
+                BottomNavigationBarItem(
+                    icon: Icon(
+                      Icons.calendar_today,
+                      color: _bottomNavigationColor,
+                    ),
+                    title: Text(
+                      'CALENDAR',
+                      style: TextStyle(
+                          color: _bottomNavigationColor,
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold),
+                    )),
+              ],
+              currentIndex: _currentIndex,
+              onTap: (int index) {
+                setState(() {
+                  _currentIndex = index;
+                });
+              },
             ),
           ),
-          Offstage(
-            offstage: !showAd,
-            child: Material(
-              child:Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(colors:[Colors.red,Colors.orange[700]]), //背景渐变
-                borderRadius: BorderRadius.circular(3.0), //3像素圆角
-                boxShadow: [ //阴影
-                  BoxShadow(
-                      color:Colors.black54,
-                      offset: Offset(2.0,2.0),
-                      blurRadius: 4.0
-                  )
-                ]
-              ),
-              child: Column(
-                mainAxisAlignment:MainAxisAlignment.center,
-                children: <Widget>[
-                  CircleAvatar(
-                    backgroundImage: NetworkImage(
-                      "https://avatars3.githubusercontent.com/u/14101776?v=4"),
-                    ),
-                  Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    alignment: Alignment.center,
-                    // decoration: BoxDecoration(
-                    //     color: Colors.white70,
-                    // ),
-                    child: Text('猫舍',style: Theme.of(context).primaryTextTheme.title),
-                  ),
-                  CountDownWidget(
-                      onCountDownFinishCallBack: (bool value) {
-                                        if (value) {
-                                          setState(() {
-                                            showAd = false;
-                                          });
-                                        }
-                                      },
-                    )
-                  ],
-              )
-            )
-            ),
-
-
-          )
-        ],
+        ),
+        Offstage(
+          offstage: !showAd,
+          child: Material(
+              child: Container(
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          colors: [Colors.red, Colors.orange[700]]), //背景渐变
+                      borderRadius: BorderRadius.circular(3.0), //3像素圆角
+                      boxShadow: [
+                        //阴影
+                        BoxShadow(
+                            color: Colors.black54,
+                            offset: Offset(2.0, 2.0),
+                            blurRadius: 4.0)
+                      ]),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            "https://avatars3.githubusercontent.com/u/14101776?v=4"),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        alignment: Alignment.center,
+                        // decoration: BoxDecoration(
+                        //     color: Colors.white70,
+                        // ),
+                        child: Text('猫舍',
+                            style: Theme.of(context).primaryTextTheme.title),
+                      ),
+                      CountDownWidget(
+                        onCountDownFinishCallBack: (bool value) {
+                          if (value) {
+                            setState(() {
+                              showAd = false;
+                            });
+                          }
+                        },
+                      )
+                    ],
+                  ))),
+        )
+      ],
     );
   }
 }
-
-
 
 class CountDownWidget extends StatefulWidget {
   final onCountDownFinishCallBack;

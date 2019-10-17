8ac2efb29b9d8b5ee639950fb3019c4baf921080
chuanwei.zhang <chuanwei.zhang@nextentertain.com>
2019-10-17 18:59:33 +0800

    api升级

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/admin/workspace_temp/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/admin/dev_custom/flutter_apistudy"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/admin/workspace_temp/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
